59af91c796b6f9a2e8763c8dd55f2212f139a160
sinius15 <sinius15@gmail.com>
2014-07-07 17:51:28 +0200

    auto commit on 07.07.2014 - 17:51:27

--- src/com/sinius15/VACM/VirtualAudioCableManager.java
+++ src/com/sinius15/VACM/VirtualAudioCableManager.java
@@ -2,12 +2,16 @@ package com.sinius15.VACM;
 
 import java.util.ArrayList;
 
+import javax.swing.JFrame;
+
 public class VirtualAudioCableManager {
 	
 	ArrayList<VirtualAudioCable> cables = new ArrayList<>();
 	
+	public VACMFrame frame;
+	
 	public VirtualAudioCableManager(){
-		
+		this.frame = new VACMFrame();
 		
 	}
 	
