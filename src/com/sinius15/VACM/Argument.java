821d7a200e71d09f520bc23e150079cadd808540
sinius15 <sinius15@gmail.com>
2014-07-07 17:27:54 +0200

    auto commit on 07.07.2014 - 17:27:54

--- src/com/sinius15/VACM/Argument.java
+++ src/com/sinius15/VACM/Argument.java
@@ -2,20 +2,28 @@ package com.sinius15.VACM;
 
 public enum Argument {
 	
-	Input("Input", String.class), 
-	Output("Output", String.class), 
-	SamplingRate("Sampling Rate", Integer.class), 
-	BitsPerSample("Bits per Sample", Integer.class), 
-	Channels("Channels", Integer.class), 
-	ChanCfg("Channel Config", String.class), 
-	BufferMs("Total Buffer (ms)", Integer.class), 
-	Buffers("Buffers", Integer.class), 
-	Priority("Priority", String.class);
+	Input("Input"), 
+	Output("Output"), 
+	SamplingRate("Sampling Rate"), 
+	BitsPerSample("Bits per Sample"), 
+	Channels("Channels"), 
+	ChanCfg("Channel Config"), 
+	BufferMs("Total Buffer (ms)"), 
+	Buffers("Buffers"), 
+	Priority("Priority");
 	
+	private String title;
 	
-	
-	Argument(String title, Class<?> type){
+	Argument(String title){
 		
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
 	}
 	
 }
