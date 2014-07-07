a39a0559f9eeb99ed86562aa04499de6a69fd7ac
sinius15 <sinius15@gmail.com>
2014-07-07 17:25:24 +0200

    auto commit on 07.07.2014 - 17:25:24

--- src/com/sinius15/VACM/Argument.java
+++ src/com/sinius15/VACM/Argument.java
@@ -4,14 +4,13 @@ public enum Argument {
 	
 	Input("Input", String.class), 
 	Output("Output", String.class), 
-	SamplingRate("Sampling Rate", ), 
-	BitsPerSample("Bits per Sample", ), 
-	Channels("Channels", ), 
-	ChanCfg("Channel Config", ), 
-	BufferMs("", ), 
-	Buffers("", ), 
-	Priority("", ), 
-	AutoStart("", );
+	SamplingRate("Sampling Rate", Integer.class), 
+	BitsPerSample("Bits per Sample", Integer.class), 
+	Channels("Channels", Integer.class), 
+	ChanCfg("Channel Config", String.class), 
+	BufferMs("Total Buffer (ms)", Integer.class), 
+	Buffers("Buffers", Integer.class), 
+	Priority("Priority", String.class);
 	
 	Argument(String title, Class<?> type){
 		
