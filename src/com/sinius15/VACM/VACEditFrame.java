313e00f19d626d3323d169d153fc8f2ab9004691
sinius15 <sinius15@gmail.com>
2014-07-07 22:16:29 +0200

    auto commit on 07.07.2014 - 22:16:29

--- src/com/sinius15/VACM/VACEditFrame.java
+++ src/com/sinius15/VACM/VACEditFrame.java
@@ -11,8 +11,9 @@ public class VACEditFrame extends JFrame {
 	private static final long serialVersionUID = -4698554134567470651L;
 	private JPanel contentPane;
 
-	public VACEditFrame() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public VACEditFrame(VirtualAudioCable cable) {
+		setTitle("Virtual Audio Cable (" +cable.getName() + ")");
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
