2bc750cd1021eb1b0ca2daeda5efdd56723020a4
sinius15 <sinius15@gmail.com>
2014-07-07 18:12:35 +0200

    auto commit on 07.07.2014 - 18:12:35

--- src/com/sinius15/VACM/VirtualAudioCableManager.java
+++ src/com/sinius15/VACM/VirtualAudioCableManager.java
@@ -13,6 +13,17 @@ public class VirtualAudioCableManager {
 		this.frame.setVisible(true);
 	}
 	
+	public void startAllCables(){
+		
+	}
+	public void stopAllCables(){
+		
+	}
+	public void restartAllCables(){
+		stopAllCables();
+		startAllCables();
+	}
+	
 	private static VirtualAudioCableManager thiss;
 	public static VirtualAudioCableManager getManager(){
 		return thiss;
