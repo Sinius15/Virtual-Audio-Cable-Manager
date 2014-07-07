edba3309f8bca3ef89eaebb9978cc59c3700513d
sinius15 <sinius15@gmail.com>
2014-07-07 17:19:53 +0200

    auto commit on 07.07.2014 - 17:19:53

---
+++ src/com/sinius15/VACM/Argument.java
@@ -0,0 +1,14 @@
+package com.sinius15.VACM;
+
+public enum Argument {
+	
+	input, 
+	output, 
+	samplerate, 
+	bitspersample, channels, chancfg, bufferms, buffers, priority, autostart;
+	
+	Argument(String title){
+		
+	}
+	
+}
--- src/com/sinius15/VACM/Arguments.java
+++
@@ -1,5 +0,0 @@
-package com.sinius15.VACM;
-
-public enum Arguments {
-	
-}
