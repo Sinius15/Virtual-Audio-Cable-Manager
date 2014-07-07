fec4d3cfbc9df93cecaf5e89e458059cefc9e8ad
sinius15 <sinius15@gmail.com>
2014-07-07 18:16:05 +0200

    auto commit on 07.07.2014 - 18:16:05

--- src/com/sinius15/VACM/VACMFrame.java
+++ src/com/sinius15/VACM/VACMFrame.java
@@ -12,7 +12,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowStateListener;
 
@@ -84,7 +83,7 @@ public class VACMFrame extends JFrame {
 				}
 			});
 			
-			MenuItem restart = new MenuItem("Restart/start all cables");
+			MenuItem restart = new MenuItem("Restart all cables");
 			restart.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
