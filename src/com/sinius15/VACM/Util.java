7a08a71d7ddc209d7782c7da1614feaea939d34f
sinius15 <sinius15@gmail.com>
2014-07-08 00:53:17 +0200

    auto commit on 08.07.2014 - 00:53:17

--- src/com/sinius15/VACM/Util.java
+++ src/com/sinius15/VACM/Util.java
@@ -1,5 +1,8 @@
 package com.sinius15.VACM;
 
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Mixer.Info;
+
 public class Util {
 	
 	public static String[] getInputDeviceNames(){
@@ -7,7 +10,23 @@ public class Util {
 	}
 	
 	public static String[] getOutputDeviceNames(){
+		Info[] mixerInfo = AudioSystem.getMixerInfo();
+		 
+		for(int i = 0; i < mixerInfo.length; i++){
+			if(mixerInfo[i].getDescription().equals())
+				System.out.println("Input: " + mixerInfo[i].getName());
+			
+				System.out.println("Output: " + mixerInfo[i].getName());
+		}
+		int amount = 0;
+		for(Info i : mixerInfo){
+			if(i.getDescription().equals("Direct Audio Device: DirectSound Playback"))
+				amount++;
+		}
 		
+		
+		return out;
 	}
 	
 }
+//"Direct Audio Device: DirectSound Capture"
