4350487f7e2735504b61d666871b40ab93a28f45
sinius15 <sinius15@gmail.com>
2014-07-07 22:15:29 +0200

    auto commit on 07.07.2014 - 22:15:29

---
+++ src/com/sinius15/VACM/VACEditFrame.java
@@ -0,0 +1,23 @@
+package com.sinius15.VACM;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class VACEditFrame extends JFrame {
+	
+	private static final long serialVersionUID = -4698554134567470651L;
+	private JPanel contentPane;
+
+	public VACEditFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+	
+}
