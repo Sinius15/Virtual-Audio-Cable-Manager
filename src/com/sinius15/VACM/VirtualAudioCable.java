2e5e605839d2d40f772243b1847aba852309811a
sinius15 <sinius15@gmail.com>
2014-07-07 20:18:23 +0200

    auto commit on 07.07.2014 - 20:18:23

--- src/com/sinius15/VACM/VirtualAudioCable.java
+++ src/com/sinius15/VACM/VirtualAudioCable.java
@@ -1,5 +1,9 @@
 package com.sinius15.VACM;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 
 public class VirtualAudioCable {
@@ -23,6 +27,23 @@ public class VirtualAudioCable {
 		this.name = windowName;
 	}
 
+	public void startAudioCable(){
+		ProcessBuilder builder = new ProcessBuilder("cmd.exe", "/c", line);
+		builder.directory(new File(System.getProperty("user.dir")));
+		builder.redirectErrorStream(true);
+		try {
+			Process p = builder.start();
+			BufferedReader r = new BufferedReader(new InputStreamReader(p.getInputStream(), "UTF-8"));
+			String l;
+			while ((l = r.readLine()) != null) {
+				CMDRepFrame.errLog("[cmd] " + l);
+			}
+			r.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public void setArgument(Argument a){
 		arguments.put(a.getTitle(), a.getValue());
 	}
