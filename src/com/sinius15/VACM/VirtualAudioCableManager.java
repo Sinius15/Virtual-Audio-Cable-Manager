8697ceb1835923c56235493349b81b8acdce8c2c
sinius15 <sinius15@gmail.com>
2014-07-07 17:04:50 +0200

    auto commit on 07.07.2014 - 17:04:50

---
+++ src/com/sinius15/VACM/VirtualAudioCableManager.java
@@ -0,0 +1,15 @@
+package com.sinius15.VACM;
+
+public class VirtualAudioCableManager {
+	
+	
+	
+	public VirtualAudioCableManager(){
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		new VirtualAudioCableManager();
+	}
+}
