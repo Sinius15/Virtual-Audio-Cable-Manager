e6bab2b9377aa8415dd0fd041764c97f446b5d2d
sinius15 <sinius15@gmail.com>
2014-07-07 22:17:29 +0200

    auto commit on 07.07.2014 - 22:17:29

--- src/com/sinius15/VACM/VACEditFrame.java
+++ src/com/sinius15/VACM/VACEditFrame.java
@@ -5,6 +5,8 @@ import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class VACEditFrame extends JFrame {
 	
@@ -14,6 +16,12 @@ public class VACEditFrame extends JFrame {
 	private VirtualAudioCable cable;
 	
 	public VACEditFrame(VirtualAudioCable cable) {
+		addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent e) {
+				
+			}
+		});
 		this.cable = cable;
 		setTitle("Virtual Audio Cable (" + cable.getName() + ")");
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
