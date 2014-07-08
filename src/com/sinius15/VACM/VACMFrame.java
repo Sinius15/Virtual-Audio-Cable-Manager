4ebffdb80e259620475e1dfc3d2ff01f1d811291
sinius15 <sinius15@gmail.com>
2014-07-08 13:30:19 +0200

    auto commit on 08.07.2014 - 13:30:19

--- src/com/sinius15/VACM/VACMFrame.java
+++ src/com/sinius15/VACM/VACMFrame.java
@@ -156,7 +156,7 @@ public class VACMFrame extends JFrame {
 		JMenuItem mntmAboutVacm = new JMenuItem("About VacM");
 		mntmAboutVacm.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				JOptionPane.showMessageDialog(thiss, "Messagegee");
+				JOptionPane.showMessageDialog(thiss, "Insert verry long about text here.", "About", JOptionPane.INFORMATION_MESSAGE);
 			}
 		});
 		mnHelp.add(mntmAboutVacm);
