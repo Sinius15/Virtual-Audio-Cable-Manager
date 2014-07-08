ac6839c0592a4652abdd4aecf3a6f558f88d7405
sinius15 <sinius15@gmail.com>
2014-07-08 14:41:50 +0200

    auto commit on 08.07.2014 - 14:41:50

--- src/com/sinius15/VACM/VirtualAudioCableManager.java
+++ src/com/sinius15/VACM/VirtualAudioCableManager.java
@@ -13,48 +13,52 @@ public class VirtualAudioCableManager {
 	
 	public VACMFrame frame;
 	
-	public VirtualAudioCableManager(){
+	public VirtualAudioCableManager() {
 		frame = new VACMFrame();
 		
 	}
 	
-	public void startAllCables(){
-		for(VirtualAudioCable cable: cables){
+	public void startAllCables() {
+		for (VirtualAudioCable cable : cables) {
 			cable.startAudioCable();
 		}
 	}
-	public void stopAllCables(){
-		for(VirtualAudioCable cable: cables){
+	
+	public void stopAllCables() {
+		for (VirtualAudioCable cable : cables) {
 			cable.stopAudioCable();
 		}
 	}
-	public void restartAllCables(){
+	
+	public void restartAllCables() {
 		stopAllCables();
 		startAllCables();
 	}
+	
 	public VirtualAudioCable getCable(String name) {
-		for(VirtualAudioCable cable: cables){
-			if(cable.getName().equals(name))
+		for (VirtualAudioCable cable : cables) {
+			if (cable.getName().equals(name))
 				return cable;
 		}
 		return null;
 	}
 	
 	private static VirtualAudioCableManager thiss;
-	public static VirtualAudioCableManager getManager(){
+	
+	public static VirtualAudioCableManager getManager() {
 		return thiss;
 	}
 	
 	public static void main(String[] args) {
-		try{
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        }catch(Exception e){
-            e.printStackTrace();
-        }
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		
 		thiss = new VirtualAudioCableManager();
 		
-		if(args.length>0){
+		if (args.length > 0) {
 			try {
 				ConfigLoader.loadData(new File(args[0]));
 			} catch (FileNotFoundException e) {
@@ -62,19 +66,17 @@ public class VirtualAudioCableManager {
 			}
 		}
 		getManager().frame.setVisible(true);
-		for(int i = 1; i < args.length; i++){
-			if(args[i].equalsIgnoreCase("autoStart"))
+		for (int i = 1; i < args.length; i++) {
+			if (args[i].equalsIgnoreCase("autoStart"))
 				getManager().startAllCables();
-			if(args[i].equalsIgnoreCase("autoStop"))
+			if (args[i].equalsIgnoreCase("autoStop"))
 				getManager().stopAllCables();
-			if(args[i].equalsIgnoreCase("quit"))
+			if (args[i].equalsIgnoreCase("quit"))
 				System.exit(0);
-			if(args[i].equalsIgnoreCase("min"))
+			if (args[i].equalsIgnoreCase("min"))
 				getManager().frame.setExtendedState(JFrame.ICONIFIED);
 		}
 		
-		
 	}
-
 	
 }
