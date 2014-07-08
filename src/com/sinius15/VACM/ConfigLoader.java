bf9e4d75b928eab5af09851725cd62885b89f1f1
sinius15 <sinius15@gmail.com>
2014-07-08 13:35:19 +0200

    auto commit on 08.07.2014 - 13:35:19

--- src/com/sinius15/VACM/ConfigLoader.java
+++ src/com/sinius15/VACM/ConfigLoader.java
@@ -1,11 +1,15 @@
 package com.sinius15.VACM;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 
 public class ConfigLoader {
 	
-	public static void loadData(File file){
+	public static Exception loadData(File file){
+		if(file.exists())
+			return new FileNotFoundException("Could not find file");
 		
+		return null;
 	}
 	
 	public static void saveData(File file){
