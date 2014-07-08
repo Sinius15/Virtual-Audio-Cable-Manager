0c892133108ca5492e08dbf17d97fad26a16ceb7
sinius15 <sinius15@gmail.com>
2014-07-08 13:33:49 +0200

    auto commit on 08.07.2014 - 13:33:49

--- src/com/sinius15/VACM/ConfigLoader.java
+++ src/com/sinius15/VACM/ConfigLoader.java
@@ -1,5 +1,14 @@
 package com.sinius15.VACM;
 
+import java.io.File;
+
 public class ConfigLoader {
 	
+	public static void loadData(File file){
+		
+	}
+	
+	public static void saveData(File file){
+		
+	}
 }
